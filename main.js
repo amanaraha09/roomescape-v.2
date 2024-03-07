17522149e953eb4098e605c6b82824a04028850f
amanaraha09 <schema51@naver.com>
2024-03-07 15:38:45 +0900

    1
    music change

--- main.js
+++ main.js
@@ -81,7 +81,8 @@ const doorfail = document.getElementById('door__fail');
 const hidden = document.querySelector("hidden");
 console.log(door1);
 
-const sound = document.getElementById("buttonsound");
+const sound = document.querySelector(".buttonsound");
+const endsound = document.querySelector("buttonsound_2");
 
 door1.addEventListener("click",function() {
     sound.play();
@@ -89,62 +90,77 @@ door1.addEventListener("click",function() {
     door2.classList.remove ('hidden');
 })
 door2.addEventListener("click",function() {
+    sound.play();
     door2.classList.add ('hidden');
     door3.classList.remove ('hidden');
 })
 door3.addEventListener("click",function() {
+    sound.play();
     door3.classList.add ('hidden');
     door4.classList.remove ('hidden');
 })
 door4.addEventListener("click",function() {
+    sound.play();
     door4.classList.add ('hidden');
     door5.classList.remove ('hidden');
 })
 door5.addEventListener("click",function() {
+    sound.play();
     door5.classList.add ('hidden');
     door6.classList.remove ('hidden');
 })
 door6.addEventListener("click",function() {
+    sound.play();
     door6.classList.add ('hidden');
     door7.classList.remove ('hidden');
 })
 door7.addEventListener("click",function() {
+    sound.play();
     // door7.classList.add ('hidden');
     // door8.classList.remove ('hidden');
 })
 door8.addEventListener("click",function() {
+    sound.play();
     door8.classList.add ('hidden');
     door9.classList.remove ('hidden');
 })
 door9.addEventListener("click",function() {
+    sound.play();
     door9.classList.add ('hidden');
     door10.classList.remove ('hidden');
 })
 door10.addEventListener("click",function() {
+    endsound.play();
     door10.classList.add ('hidden');
     doorfail.classList.remove ('hidden');
 })
 door11.addEventListener("click",function() {
+    sound.play();
     door11.classList.add ('hidden');
     door12.classList.remove ('hidden');
 })
 door12.addEventListener("click",function() {
+    sound.play();
     door12.classList.add ('hidden');
     door13.classList.remove ('hidden');
 })
 door13.addEventListener("click",function() {
+    sound.play();
     door13.classList.add ('hidden');
     door14.classList.remove ('hidden');
 })
 door14.addEventListener("click",function() {
+    endsound.play();
     door14.classList.add ('hidden');
     doorfail.classList.remove ('hidden');
 })
 door15.addEventListener("click",function() {
+    sound.play();
     door15.classList.add ('hidden');
     door16.classList.remove ('hidden');
 })
 door16.addEventListener("click",function() {
+    endsound.play();
     door16.classList.add ('hidden');
     doorfail.classList.remove ('hidden');
 })
